aee9edb1ed9f88afb51d4601bb9478d38151919d
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-06-18 20:15:03 -0300

    middleware de autenticacao

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::controller(AuthController::class)->group(function () {
-  Route::get('/login', 'geraPaginaLogin');
-  Route::get('/cadastro', 'geraPaginaCadastro');
-  Route::post('/login', 'login')->name('login');
-  Route::post('/cadastro', 'cadastro')->name('cadastro');
-  Route::post('/logout', 'logout')->name('logout');
+Route::middleware(['guest'])->group(function () {
+  Route::controller(AuthController::class)->group(function () {
+    Route::get('/login', 'geraPaginaLogin');
+    Route::get('/cadastro', 'geraPaginaCadastro');
+    Route::post('/login', 'login')->name('login');
+    Route::post('/cadastro', 'cadastro')->name('cadastro');
+  });
+});
+
+Route::middleware(['auth'])->group(function () {
+  Route::controller(AuthController::class)->group(function () {
+    Route::post('/logout', 'logout')->name('logout');
+  });
+
+  Route::get('/', function () {
+    return view('pages.home');
+  });
 });
