cf2801124979102236b9ac784f4beb5da9c4b427
marinacjensenn <marina.jensen@aluno.riogrande.ifrs.edu.com>
2023-07-03 11:58:42 -0300

    criação chaves pix

---
+++ resources/views/pages/chavespix.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('main')
+  <main class="flex-grow bg-orange-50 grid grid-cols-2 place-items-center">
+    <h2 class="mb-7 text-5xl font-light">Cadastrar chaves</h2>
+    <div class="w-96">
+      <form action="{{ route('cadastraChavePix') }}" method="post">
+        @csrf
+        <div class="space-y-3 mb-7">
+          <div class="-space-y-1">
+            <label for="cpf" class="block text-sm">CPF:</label>
+            <input type="text" name="cpf" id="cpf" class="bg-transparent outline-none block py-1 w-full border-b-2 border-orange-500 text-lg"  autofocus>
+          </div>
+          <div class="-space-y-1">
+            <label for="telefone" class="block text-sm">Telefone:</label>
+            <input type="text" name="telefone" id="telefone" class="bg-transparent outline-none block py-1 w-full border-b-2 border-orange-500 text-lg">
+          </div>
+          <div class="-space-y-1">
+            <label for="email" class="block text-sm">E-mail:</label>
+            <input type="email" name="email" id="email" class="bg-transparent outline-none block py-1 w-full border-b-2 border-orange-500 text-lg">
+          </div>
+        </div>
+        <div class="grid grid-cols-2">
+          <a href="{{ url('/') }}" class="block place-self-center text-orange-500 underline">Voltar</a>
+          <button type="submit" class="block h-12 bg-orange-500 text-white font-bold rounded-sm shadow-sm">Pagar</button>
+        </div>
+      </form>
+    </div>
+  </main>
+@endsection
