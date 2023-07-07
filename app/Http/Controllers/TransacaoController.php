6f554e7f80719ba655135542c73684781cc9f747
pedromchd <pedrogmachado28@outlook.com>
2023-07-07 19:15:10 -0300

    operacoes

--- app/Http/Controllers/TransacaoController.php
+++ app/Http/Controllers/TransacaoController.php
@@ -40,40 +40,49 @@ class TransacaoController extends Controller
     $limit = Auth::user()->contas->limit;
     $fatura = Auth::user()->contas->fatura;
     $valor = $request->valor;
+    $tipo = $request->tipo;
 
-    if ($request->tipo === 'Depósito') {
+    if ($tipo === 'Depósito') {
       $saldo += $valor;
     }
-    else if ($request->tipo === 'Fatura' && $valor <= $fatura) {
-      if ($valor <= $saldo) {
-        $saldo -= $valor;
-        $fatura -= $valor;
+    if ($tipo === 'Fatura') {
+      if ($valor <= $fatura) {
         $limit += $valor;
-      } else {
-        return back();
+        $fatura -= $valor;
       }
+      return back()->withErrors([
+        'erro' => 'O valor informado é maior que o da fatura',
+      ]);
     }
-    else if ($request->tipo !== 'Crédito') {
+    if ($tipo === 'Boleto' || $tipo === 'Débito') {
       if ($valor <= $saldo) {
         $saldo -= $valor;
-      }
-      else if ($valor > $saldo && $request->limite === 'off') {
-        return back();
-      }
-      else if ($valor > $saldo && $request->limite === 'on' && $valor - $saldo <= $limit) {
-        $fatura += $saldo + ($valor - $saldo) * 1.01;
+      } else
+      if ($request->limite === 'On') {
         $saldo = 0;
-        $limit -= ($valor - $saldo) * 1.01;
+        $valor -= $saldo;
+        if ($valor <= $limit) {
+          $limit -= $valor * 1.01;
+          $fatura += $valor * 1.01;
+        } else {
+          return back()->withErrors([
+            'erro' => 'Você não tem limite para realizar esta operação',
+          ]);
+        }
       } else {
-        return back();
+        return back()->withErrors([
+          'erro' => 'Você não tem saldo para realizar esta operação',
+        ]);
       }
     }
-    else {
+    if ($tipo === 'Crédito') {
       if ($valor <= $limit) {
-        $fatura += $saldo + ($valor - $saldo) * 1.01;
-        $limit -= ($valor - $saldo) * 1.01;
+        $limit -= $valor * 1.01;
+        $fatura += $valor * 1.01;
       } else {
-        return back();
+        return back()->withErrors([
+          'erro' => 'Você não tem limite para realizar esta operação',
+        ]);
       }
     }
 
